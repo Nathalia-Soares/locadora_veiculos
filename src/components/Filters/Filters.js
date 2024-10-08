5127147f040e0b8cf4b6bf9c5a4cc6dc2050ac6c
Nathalia-Soares <nathalia.oliveira24@fatec.sp.gov.br>
2024-10-08 12:52:15 -0300

    refactoring mouseflow script

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -8,22 +8,13 @@ const Filters = () => {
   useEffect(() => {
     // Carregar Mouseflow
     mouseflow.initialize('f69f3fb0-f0bd-4160-b813-e6410a4b054f'); // Substitua pelo seu ID do projeto Mouseflow
-    document.head.appendChild(mouseflow);
-
-    // Definir variante A ou B (de forma aleatória)
-    const selectedVariant = Math.random() < 0.5 ? 'A' : 'B';
-    setVariant(selectedVariant);
-
-    mouseflow.newPageView('/');
-    // Adicionar tag do Mouseflow para rastrear a variante
-    if (mouseflow.initialized()) {
-      mouseflow.tag('tag', `FiltersVariant${selectedVariant}`);
-    }
-
-    return () => {
-      document.head.removeChild(mouseflow);
-    };
   }, []);
+
+  mouseflow.newPageView('/');
+  // Adicionar tag do Mouseflow para rastrear a variante
+  if (mouseflow.initialized()) {
+    mouseflow.tag('tag', `FiltersVariant${selectedVariant}`);
+  }
 
   const handleButtonClick = (button) => {
     // Rastreia o clique nos botões usando o Mouseflow
