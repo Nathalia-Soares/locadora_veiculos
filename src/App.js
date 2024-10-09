65349884dc8ace13f8a0e9bdf67a75a56dbd3d3d
Nathalia-Soares <nathalia.oliveira24@fatec.sp.gov.br>
2024-10-09 00:09:44 -0300

    Including HotJar scripts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Filters from './components/Filters/Filters';
 import StoreList from './components/StoreList/StoreList';
-import Hotjar from "./views/helpers/Hotjar.js";
+import Hotjar from "./helpers/hotjar";
 import './App.css';
 
 function App() {
