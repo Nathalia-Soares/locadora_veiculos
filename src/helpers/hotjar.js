62fde2bf155889c23475e67bad0104d16f8cf315
Nathalia-Soares <nathalia.oliveira24@fatec.sp.gov.br>
2024-10-09 01:35:13 -0300

    Including HotJar scripts

--- src/helpers/hotjar.js
+++ src/helpers/hotjar.js
@@ -7,7 +7,7 @@ const Hotjar = () => {
         script.innerHTML = `
             (function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings=hjid:${process.env.HOTJAR_ID},hjsv:6};
+                h._hjSettings=hjid:${SECRETS.HOTJAR_ID},hjsv:6};
                 a=o.getElementsByTagName('head')[0];
                 r=o.createElement('script');r.async=1;
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
