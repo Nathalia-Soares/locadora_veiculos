2006f1eb7a1360d8384c0490f47791a3e68aa763
Nathalia-Soares <nathalia.oliveira24@fatec.sp.gov.br>
2024-10-09 01:53:46 -0300

    Including Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Header from './components/Header/Header';
 import Filters from './components/Filters/Filters';
 import StoreList from './components/StoreList/StoreList';
 import Hotjar from "./helpers/hotjar";
+import { Analytics } from "@vercel/analytics/react"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Analytics/>
       <Header />
       <Filters />
       <Hotjar />
